96d4ff904d4bda0e07025b74e86e6c26da3a6e01
Jervie <jdmarquez@iremit-inc.com>
2025-02-27 01:20:59 +0800

    base files

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Dashboard } from '@/components/Dashboard/Dashboard'
+import { Sidebar } from '@/components/Sidebar/Sidebar'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='grid gap-4 p-4 grid-cols-[220px,_1fr]'>
+        <Sidebar />
+        <Dashboard />
+    </main>
+  )
+}
+
+export default Home
